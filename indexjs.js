6369e9e2284a69d08480ef64a5e5d23a3efba70d
Akimov-Ihor <57266437+Akimov-Ihor@users.noreply.github.com>
2020-03-19 18:53:04 +0200

    [Add]Line-through button work

--- indexjs.js
+++ indexjs.js
@@ -2,6 +2,7 @@ let arr = []
 let containerToDo = document.getElementById('listCreate');
 let listButton = document.getElementById('button-cheaker');
 let linesOfTodolist = document.getElementsByClassName('todoItem');
+// let SpanTest=document.getElementsByClassName("SpanWithArrValue")
 
 const addToDo = () => {
     let todoObj = {
@@ -20,18 +21,24 @@ const addToDo = () => {
     let crtSpan = document.createElement("Span");
     crtSpan.innerHTML = arr[arr.length - 1].value;
     element.appendChild(crtSpan);
+    crtSpan.id=new Date().getTime();
+    crtSpan.classList.add('SpanWithArrValue')
+    
     
     let checkingButton = document.createElement('input');
     checkingButton.type = "checkbox";
     checkingButton.value = false;
+    checkingButton.id=new Date().getTime();
     checkingButton.classList.add('button-cheaker');
+    checkingButton.onclick = () => overlineCheck(checkingButton,crtSpan)
+    
 
     let buttTrash = document.createElement('button');
     buttTrash.onclick = () => deleteListOfToDo(todoObj.id)
     buttTrash.classList.add('buttonForTrash')
-
-    crtSpan.appendChild(checkingButton)
-    crtSpan.appendChild(buttTrash)
+    
+    element.appendChild(checkingButton)
+    element.appendChild(buttTrash)
 
 }
 
@@ -40,3 +47,14 @@ let deleteListOfToDo = (id) => {
     let divForDelete = document.getElementById(`todo-id-${id}`);
     divForDelete.parentNode.removeChild(divForDelete);
 };
+
+
+function overlineCheck(e,span){
+    if( e.checked !== false){
+        span.style.textDecorationLine="line-through";
+    }
+    else{
+        span.style.textDecorationLine='none';
+    }
+}
+
